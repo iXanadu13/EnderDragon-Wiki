be7f72ecb40a2576c396d1c44581e8ffeeb48159
Xanadu13 <104714710+iXanadu13@users.noreply.github.com>
2023-07-25 11:16:58 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/intro">
-            点击查看文档
+            Start!
           </Link>
         </div>
       </div>
